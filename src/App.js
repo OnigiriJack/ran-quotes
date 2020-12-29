b07a9cf987a0fe2dd267d0469f89b63bb04676e7
Jack Fowler <OnigiriJack@github.com>
2020-12-29 18:26:38 +0900

    added clickable button and working link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function handleClick() {
+    alert("button was clicked");
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +13,8 @@ function App() {
         <div id="quote-box">
           <p id="text">I am text inside of quote box</p>
           <p id="author"> I am an author inside this box </p>
+          <button id="new-quote" onClick={handleClick}>new quote</button>
+          <a id="tweet-quote" href="https://google.com">i am clickable</a>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
