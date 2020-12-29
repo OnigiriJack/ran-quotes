3b151fcfbfe55f0b3994e66048636a9f4efa22f8
Jack Fowler <OnigiriJack@github.com>
2020-12-29 18:27:13 +0900

    able to use fetch to rerender screen with different quotes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
 
-  function handleClick() {
-    alert("button was clicked");
+  const [quoteInfo, setQuoteInfo] = useState({
+    quote: "Knowing is not enough; we must apply!",
+    author: "Johann Wolfgang von Goethe"
+  });
+  function getQuote() {
+    fetch("https://api.quotable.io/random")
+      .then(function (resp) {
+        return resp.json();
+      })
+      .then(function (data) {
+        setQuoteInfo({
+          quote: data.content,
+          author: data.author
+        })
+        console.log(data);
+      });
+    // fetch("https://quotes.rest/quote/random?language=en&limit=1")
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+
   }
   return (
     <div className="App">
       <header className="App-header">
 
         <div id="quote-box">
-          <p id="text">I am text inside of quote box</p>
-          <p id="author"> I am an author inside this box </p>
-          <button id="new-quote" onClick={handleClick}>new quote</button>
+          <p id="text">  {quoteInfo.quote}</p>
+          <p id="author"> {quoteInfo.author} </p>
+          <button id="new-quote" onClick={getQuote}>new quote</button>
           <a id="tweet-quote" href="https://google.com">i am clickable</a>
         </div>
         <img src={logo} className="App-logo" alt="logo" />
