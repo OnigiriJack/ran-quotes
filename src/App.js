69a30b0a37d7213f06b71a858e8d6b9a5c24a793
Jack Fowler <OnigiriJack@github.com>
2020-12-29 20:44:58 +0900

    Need to find some Japanese quotes to scrape

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useEffect, useState } from 'react';
-
+import { TwitterShareButton } from 'react-twitter-embed';
 
 function App() {
 
   const [quoteInfo, setQuoteInfo] = useState({
 
   });
-
+  // need to remove whitespace from author
   useEffect(() => {
     getQuote();
     //if I added a state varibale in the array the app will rerender when it senses the change of the value
@@ -33,15 +33,13 @@ function App() {
       <header className="App-header">
 
         <div id="quote-box">
+
           <p id="text">  {quoteInfo.quote}</p>
           <p id="author"> {quoteInfo.author} </p>
           <button id="new-quote" onClick={getQuote}>new quote</button>
-          <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${quoteInfo.quote} - ${quoteInfo.author}`} >i am clickable</a>
+
+          <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${quoteInfo.quote} &hashtags=${quoteInfo.author},FCC,React`} >Tweet Me</a>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>asdfasf sfasf asf and save to reload.
-        </p>
 
       </header>
     </div >
